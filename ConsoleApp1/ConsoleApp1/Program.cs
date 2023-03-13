96d9e7e3b96324fe294e2a2c1c133e9a82e07739
Jozef Paszkiewicz <paszkiewicz.jozef@gmail.com>
2023-03-13 21:37:47 +0100

    Zadanie dzień 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var woman = true;
+var age = 30;
+
+if (woman == true)
+{
+    if (age < 30){
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+} 
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Mężczyzna, lat "+age);
+    }
+}
+Console.WriteLine("Cześć, mam na imię " + name + " i mam " + age + " lat.");
